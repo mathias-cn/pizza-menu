ec4b2d897d926dad69dfccc14052116786bc47e4
Mathias Navarre <mathiascnavarre@gmail.com>
2024-07-13 23:50:09 -0300

    ...

--- src/page/components/footer.tsx
+++ src/page/components/footer.tsx
@@ -9,7 +9,7 @@ export function Footer() {
         <footer> 
                 {currentHour > workingHours.open && currentHour < workingHours.close 
                 ? (<p className="text-3xl">{new Date().toLocaleTimeString()} - We're currently open!</p>) 
-                : (<p className="text-3xl text-red-500">{new Date().toLocaleTimeString()} - We're closed. Comeback at ${workingHours.open} hours</p>)
+                : (<p className="text-3xl text-red-500">{new Date().toLocaleTimeString()} - We're closed. Comeback at {workingHours.open} hours</p>)
                 }
         </footer>
     )
